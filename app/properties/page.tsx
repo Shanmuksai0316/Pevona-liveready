53c046f22bcddca66bbbf7846cfecbdc173d9a13
Nagraj Y R <nagrajyr786@gmail.com>
2025-12-31 14:51:16 +0530

    Reduce left and right padding on All Properties page and filter section, make it responsive

--- app/properties/page.tsx
+++ app/properties/page.tsx
@@ -6,7 +6,7 @@ export default async function PropertiesPage() {
   const properties = await fetchStrapi<StrapiProperty[]>("/api/properties?populate=*");
 
   return (
-    <div className="container mx-auto px-4 pt-[100px] sm:pt-[120px] lg:pt-[140px] pb-16">
+    <div className="container mx-auto px-4 sm:px-6 md:px-8 lg:px-12 xl:px-16 pt-[100px] sm:pt-[120px] lg:pt-[140px] pb-16">
       <h1 className="font-crimson text-5xl font-bold text-pevona-dark mb-8">
         All Properties
       </h1>
